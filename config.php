fe7b1ef56c43a67cb8d240229665f190fdf91eaf
HIDRAN ARIAS <hidran.arias@eng.it>
2018-08-25 15:28:28 +0200

    add mysql config file and connection file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'mysql_host' => 'localhost',
+    'mysql_user' => 'root',
+    'mysql_password' => 'hidran',
+    'mysql_db' => 'corsophp'
+];
+
---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$config = require_once 'config.php';
+
+
+$mysqli = new mysqli(
+    $config['mysql_host'],
+    $config['mysql_user'],
+    $config['mysql_password'],
+    $config['mysql_db']
+);
+
+ unset($config );
+
+ if($mysqli->connect_error){
+     die($mysqli->connect_error);
+ }
+ 
