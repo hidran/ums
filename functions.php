63ff101891af492724eb1c4d8ed5f685a41c1731
HIDRAN ARIAS <hidran.arias@eng.it>
2018-08-25 15:58:34 +0200

    add random functions to calculate fiscalcode, names, age and emails

---
+++ functions.php
@@ -0,0 +1,47 @@
+<?php
+
+
+    function getRandName() {
+        $names = [
+            'ROBERTO','GIOVANNI','GIULIA','MARIO','ALE'
+        ];
+        $lastnames = [
+            'ROSSI','RE','ARIAS','SMITH','MENDOZA','CRUZ','WILDE'
+
+        ];
+
+        $rand1 =  mt_rand(0, count($names) -1) ;
+        $rand2 =  mt_rand(0, count($lastnames) -1);
+
+      return  $names[$rand1].' '. $lastnames[$rand2] ;
+    }
+
+    //echo getRandName();
+    function getRandEmail($name){
+
+        $domains = ['google.com','yahoo.com','hotmail.it', 'libero.it'];
+
+        $rand1 =  mt_rand(0, count($domains) -1) ;
+
+        return  strtolower(str_replace(' ','.',$name ).mt_rand(10,99).'@'.$domains[$rand1]);
+
+    }
+    function getRandFiscalCode(){
+
+        $i = 16;
+        $res = '';  // ABQZ
+
+        while ( $i > 0){
+
+            $res .= chr(mt_rand(65,90));
+
+            $i--;
+
+        }
+        return $res;
+
+    }
+    function getRandomAge(){
+         return mt_rand(0, 120);
+    }
+    echo getRandomAge();
